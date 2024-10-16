b1bd04db0de946aff7fe7d595643e267cedc2949
Anastasiia Balieva <anastasiya.balieva@myoffice.team>
2024-10-17 02:30:23 +0300

    dz6_try1

---
+++ tests/conftest.py
@@ -0,0 +1,43 @@
+import os.path
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+import pytest
+
+
+def pytest_addoption(parser):
+    parser.addoption("--browser", default="chrome")
+    parser.addoption("--yad", default="/Users/user/drivers/yandexdriver")
+    parser.addoption("--base_url", default="http://192.168.0.110:8081/")
+    parser.addoption("--drivers", default=os.path.expanduser("/Users/user/drivers/yandexdriver"))
+
+
+@pytest.fixture
+def base_url(request):
+    return request.config.getoption("--base_url")
+
+
+@pytest.fixture
+def browser(pytestconfig):
+    browser_name = pytestconfig.getoption("browser")
+    yad = pytestconfig.getoption("yad")
+    driver = None
+
+    if browser_name in ["cr", "chrome"]:
+        driver = webdriver.Chrome()
+    elif browser_name in ["ff", "firefox"]:
+        driver = webdriver.Firefox()
+    elif browser_name in ["ya", "yandex"]:
+        options = ChromeOptions()
+        options.binary_location = "/Applications/Yandex.app"
+        driver = webdriver.Chrome(options=options, service=Service(executable_path=yad))
+    yield driver
+    driver.quit()
+
+
+
+
+
+
+
